da34cd6f66ce0ec66fd511702bb1fccc004d84a1
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-07-11 12:25:10 +0200

    Added exception handling to ParseInts.java

--- Problem4/ParseInts.java
+++ Problem4/ParseInts.java
@@ -9,19 +9,22 @@ public class ParseInts
 {
     public static void main(String[] args)
     {
-        int val, sum=0;
+        int val, sum = 0;
         Scanner scan = new Scanner(System.in);
-        String line;
 
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
 
         while (scanLine.hasNext())
         {		
-            val = Integer.parseInt(scanLine.next());
-            sum += val;
+            try {
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            } catch (NumberFormatException e) {
+                // Empty catch block to handle non-integer tokens
+            }
         }
+
         System.out.println("The sum of the integers on this line is " + sum);
     }
-
 }
